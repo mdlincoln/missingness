c649294b6c4d77e793c6db4cd0d4c294d7520115
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-09-07 16:57:13 -0700

    document addt'l vector variables

--- global.R
+++ global.R
@@ -6,12 +6,14 @@ kg <- knoedler %>%
   select(pi_record_no, genre, sale_date_year, buy_auth_name_1) %>%
   filter(!is.na(sale_date_year) & sale_date_year < 1970)
 
+# Find the current distribution of genres in the original data
 genres <- kg %>%
   na.omit() %>%
   count(genre) %>%
   mutate(prob = n/sum(n)) %>%
   arrange(genre)
 
+# Make these genres and their proportions available as vectors
 gnames <- genres$genre
 gstart <- genres$prob
 names(gstart) <- gnames
