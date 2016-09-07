72b0308fb6af355dfa1dcf4d9b99fc0834b3736e
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-09-07 16:53:43 -0700

    Set genre prob defaults to the overall occurrence in orig data

--- global.R
+++ global.R
@@ -6,6 +6,12 @@ kg <- knoedler %>%
   select(pi_record_no, genre, sale_date_year, buy_auth_name_1) %>%
   filter(!is.na(sale_date_year) & sale_date_year < 1970)
 
+genres <- kg %>%
+  na.omit() %>%
+  count(genre) %>%
+  mutate(prob = n/sum(n)) %>%
+  arrange(genre)
 
-
-gnames <- sort(na.omit(unique(kg$genre)))
+gnames <- genres$genre
+gstart <- genres$prob
+names(gstart) <- gnames
