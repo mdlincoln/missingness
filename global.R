83285d0d02ce972886c349e2ed1ee2fb60c7a496
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-09-07 14:19:49 -0700

    restrict daterange to pre-1970

--- global.R
+++ global.R
@@ -4,7 +4,7 @@ library(dplyr)
 # Resample possible knoedler genres
 kg <- knoedler %>%
   select(pi_record_no, genre, sale_date_year, buy_auth_name_1) %>%
-  filter(!is.na(sale_date_year))
+  filter(!is.na(sale_date_year) & sale_date_year < 1970)
 
 
 
